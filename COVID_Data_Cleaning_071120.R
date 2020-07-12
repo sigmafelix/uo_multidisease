a640cec82d84ffdfa7d90f0cb2b1369ec941e4aa
Insang Song <sigmafelix@hotmail.com>
2020-07-11 22:49:14 -0700

    COVID data update

---
+++ COVID_Data_Cleaning_071120.R
@@ -0,0 +1,43 @@
+## data munging
+
+library(pacman)
+p_load(tidyverse, sf, dtplyr)
+
+pdir <- 'C:/Users/sigma/OneDrive/Data/HIV/Multidisease_data/'
+
+# files
+covid_oh <- read_csv(str_c(pdir, 'COVID_Ohio_071120.csv'), col_types = 'ccccccddd')
+covid_ms <- read_csv(str_c(pdir, 'COVID_Mississippi_063020.csv'))
+covid_us <- read_csv(str_c(pdir, 'COVID_County_063020.csv'))
+
+# filtering
+covid_oh <- covid_oh %>% 
+  mutate(death_date = lubridate::mdy(`Date Of Death`),
+         State = 'Ohio') %>% 
+  filter(death_date <= as.Date('2020-06-30') | (`Death Count` != 0)) %>% 
+  rename(Age_range = `Age Range`,
+         NDeath = `Death Count`) %>% 
+  dplyr::select(State, County, Sex, Age_range, NDeath, death_date) %>% 
+  group_by(State, County, Sex, Age_range) %>% 
+  summarize(NDeath = sum(NDeath)) %>% 
+  ungroup
+
+covid_ms <- covid_ms %>% 
+  pivot_longer(cols = 3:21, names_to = c('Hispanic', 'Ethnicity'), names_sep = '_') %>% 
+  mutate(Hispanic = plyr::mapvalues(Hispanic, c('NH', 'H', 'UN'), c('Non-Hispanic', 'Hispanic', 'Unknown')),
+         Ethnicity = plyr::mapvalues(Ethnicity, c('Afr', 'W', 'AIAN', 'As', 'other', 'unk'),
+                                     c('African-American', 'White', 'American-Indian-Alaska-Native', 'Asian', 'Other', 'Unknown')),
+         County = str_c(str_sub(County, 1, 1), tolower(str_sub(County, 2, -1))))
+
+covid_us <- covid_us %>% 
+  filter(Country_Region == 'US') %>% 
+  rename(State = Province_State,
+         County = Admin2,
+         NDeath = Deaths) %>% 
+  mutate(FIPS = sprintf('%05d', FIPS)) %>% 
+  dplyr::select(FIPS, State, County, NDeath)
+
+# export data
+write_csv(covid_oh, str_c(pdir, 'COVID_Ohio_Cleaned_063020.csv'))
+write_csv(covid_ms, str_c(pdir, 'COVID_Mississippi_Cleaned_063020.csv'))
+write_csv(covid_us, str_c(pdir, 'COVID_County_Cleaned_063020.csv'))
---
+++ Readme_COVID.txt
@@ -0,0 +1,24 @@
+COVID data (State/stratified) readme
+
+Maintainer: Insang Song (isong@uoregon.edu)
+Effective date: 06/30/2020 for all data
+
+* Ohio data
+State
+County
+Sex
+Age_range
+NDeath: Death counts
+
+* Mississippi
+State
+County
+Ishispanic: Hispanic (Hispanic, Non-Hispanic, Unknown)
+Ethnicity: African-American, White, American-Indian and Alaska Natives, Asian, Other, Unknown
+NDeath: Death counts
+
+* US County
+FIPS: 5-digit county code
+State
+County
+NDeath: Death counts
