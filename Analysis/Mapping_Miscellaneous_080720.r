71e5655cdedbd2337cf661b2f2b5af28830fbf4a
Insang Song <sigmafelix@hotmail.com>
2020-08-15 15:19:05 -0700

    Code update (081520)

--- Analysis/Mapping_Miscellaneous_080720.r
+++ Analysis/Mapping_Miscellaneous_080720.r
@@ -1,6 +1,6 @@
-### last revision: 081020
+### last revision: 081520
 library(pacman)
-p_load(tidyverse, sf, spdep, dtplyr, tmap, rmapshaper)
+p_load(tidyverse, sf, spdep, dtplyr, tmap, rmapshaper, classInt)
 
 county_s <- st_read('/mnt/c/Users/sigma/OneDrive/Data/Geo/tl_2018_us_county.shp') %>% 
     st_transform(2163)
@@ -30,21 +30,54 @@ county_merge <- county_s %>%
 county_merge_s <- county_merge %>% 
   ms_simplify(keep = 0.1, method = 'dp')
 
+write_csv(county_merge_s %>% st_set_geometry(NULL),
+          '/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Result/County_Merged_Attributes_081020.csv')
+st_write(county_merge_s, '/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Result/County_Merged_081020.geojson')
+st_write(county_merge_s[,'GEOID'], '/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Result/County_Base.shp')
+
+
+## Complex mapping
+abbr <- read_csv('/mnt/c/Users/sigma/OneDrive/Data/Geo/State_Abbreviation.csv') %>% 
+  mutate(FIPS = sprintf('%02d', FIPS_State))
+
+
+
 ### Misc functions
-mapping_select <- function(dat, field_name, basepath){
-    tms <- tm_shape(dat) +
-        tm_fill(col = field_name, palette = 'Reds', n = 7) +
+mapping_select <- function(dat, field_name, basepath, fips = abbr){
+    cut_class <- classIntervals(dat %>% st_set_geometry(NULL) %>% .[,field_name] %>% as.vector,
+                                n= 7, style = 'jenks')$brks
+    dat_cont <- dat %>% filter(STATEFP %in% (fips %>% filter(Contiguous == 1) %>% .$FIPS))
+    dat_ak <- dat %>% filter(STATEFP == '02')
+    dat_hi <- dat %>% filter(STATEFP == '15')
+    tms <- tm_shape(dat_cont) +
+        tm_fill(col = field_name, palette = 'Reds', breaks = cut_class) +
         tm_borders(col = 'transparent') +
-        tm_layout(legend.show = TRUE, legend.position = c('LEFT', 'BOTTOM'))
+        tm_layout(legend.show = TRUE, legend.position = c('RIGHT', 'BOTTOM'), frame = FALSE)
+    tms_ak <- tm_shape(dat_ak) +
+        tm_fill(col = field_name, palette = 'Reds', breaks = cut_class) +
+        tm_borders(col = 'transparent') +
+        tm_layout(legend.show = FALSE, frame = FALSE, bg.color = NA)
+    tms_hi <- tm_shape(dat_hi) +
+        tm_fill(col = field_name, palette = 'Reds', breaks = cut_class) +
+        tm_borders(col = 'transparent') +
+        tm_layout(legend.show = FALSE, frame = FALSE, bg.color = NA)
     fullpath <- str_c(basepath, field_name, '.png')
     if (!is.null(basepath)){
-    tmap_save(tm = tms, filename = fullpath,
-            height = 15, width = 24, units = 'cm', dpi = 300)
+    png(filename = fullpath,
+        height = 15, width = 24, units = 'cm', res = 300)
+      print(tms)
+      print(tms_hi, vp = grid::viewport(0.25, 0.125, width = 0.15, height = 0.25))
+      print(tms_ak, vp = grid::viewport(0.1, 0.12, width = 0.25, height = 0.25))
+    dev.off()
+    #tmap_save(tm = tms, filename = fullpath,
+    #        height = 15, width = 24, units = 'cm', dpi = 300)
     } else {
-      tms
+      print(tms)
+      print(tms_hi, vp = grid::viewport(0.25, 0.125, width = 0.15, height = 0.25))
+      print(tms_ak, vp = grid::viewport(0.1, 0.12, width = 0.25, height = 0.25))
     }
 }
-
+#abbr %>% filter(Contiguous == 1) %>% .$FIPS
 #mapping_select(county_merge_s, 'MAX', NULL)
 
 mi_select <- function(dat, field_name){
@@ -68,25 +101,14 @@ lmi_select <- function(dat, field_name, na.action = na.pass){
 
 }
 
-county_merge_s <- county_merge_s %>% 
-  dplyr::select(-87:-89, -92:-93) %>% 
-  mutate_at(.vars = vars(alcohol, incarceration, overdose, black_insurance, readmission,
-                         pop_SNAP, primary),
-            .funs = list(~ifelse(.=='N/A', NA, as.numeric(.))))
-for (i in 5:88){mapping_select(county_merge_s, colnames(county_merge_s)[i], basepath = '/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Maps/Values/')}
-for (i in 5:88){
-  lmid <- lmi_select(county_merge_s, colnames(county_merge_s)[i])
-  png(str_c('/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Maps/LocalMoranI/', colnames(county_merge_s)[i], '.png'),
-      width = 25, height = 16, units = 'cm', res = 300)
-  plot.local.moran(county_merge_s, colnames(county_merge_s)[i], lmid, clw)
-  dev.off()
-  }
-
+for (i in 8:8){mapping_select(county_merge_s, colnames(county_merge_s)[i], basepath = '/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Maps/')}
 
 #save(plot.local.moran, lmi_select, mi_select, mapping_select, clw, county_merge_s, file = '/mnt/c/Users/sigma/OneDrive/Data/HIV/Dashboard_base.RData', compress = 'xz')
 
 ## Courtsey of https://github.com/gisUTM/spatialplots/blob/master/plotlocalmoran.R
-plot.local.moran <- function(x, variable.name, local.moran, weights, sig = 0.05, plot.only.significant = TRUE, legend.location = "bottomleft", zero.policy = NULL){
+plot.local.moran <- function(x, variable.name, local.moran, weights, sig = 0.05, 
+                            plot.only.significant = TRUE, legend.location = "bottomleft", zero.policy = NULL,
+                            fips = abbr, basepath=NULL){
   if(!inherits(local.moran, "localmoran"))
     stop("local.moran not an object of class localmoran")
   if(!inherits(weights, "listw"))
@@ -105,16 +127,13 @@ plot.local.moran <- function(x, variable.name, local.moran, weights, sig = 0.05,
     
     # Subset localmoran
     local.moran <- local.moran[subVec,]
-    
     # Subset Polygons
     origPoly <- x
     #x <- subset(x, subVec)
   }
   origPoly <- x
-  x <- x %>% st_set_geometry(NULL)
   # Get length of x
   n <- nrow(x)
-  
   #
   vec <- c(1:n)
   vec <- ifelse(local.moran[,5] < sig, 1,0)
@@ -150,7 +169,7 @@ plot.local.moran <- function(x, variable.name, local.moran, weights, sig = 0.05,
   colors <- c(1:n)
   for (i in 1:n) {
     if (is.na(q.all[i])){
-      colors[i] <- 'black'
+      colors[i] <- NA
     } else {
     if (q.all[i]==1) 
       colors[i] <- "red"
@@ -159,11 +178,11 @@ plot.local.moran <- function(x, variable.name, local.moran, weights, sig = 0.05,
     if (q.all[i]==3) 
       colors[i] <- "lightblue"
     if (q.all[i]==4) 
-      colors[i] <- "pink"
-    if (q.all[i]==0) 
-      colors[i] <- "white"   
-    if (q.all[i]>4) 
-      colors[i] <- "white"
+      colors[i] <- "pink2"
+    #if (q.all[i]==0) 
+    #  colors[i] <- "white"   
+    #if (q.all[i]>4) 
+    #  colors[i] <- "white"
   }
   }
   
@@ -172,9 +191,7 @@ plot.local.moran <- function(x, variable.name, local.moran, weights, sig = 0.05,
   colors1 <- colors
   for (i in 1:n){
     if ( !(is.na (locm.dt[i])) )  {
-
       if (locm.dt[i]==0) colors1[i] <- "grey78"
-
     }
   }
   
@@ -191,18 +208,118 @@ plot.local.moran <- function(x, variable.name, local.moran, weights, sig = 0.05,
   pink0 <- which(colors2=="pink 0")
   lb <- 6
   labels=c("High-High", "High-Low", "Low-High", "Low-Low", "Not Significant", "Missing Data")
+  
+  #x$LISA_sig <- as.factor(q)
+  x$LISA_sig <- factor(colors1, levels = c('red', 'pink2', 'lightblue', 'blue', 'grey78'))
+  #lisa_levels = c('red', 'blue', 'lightblue', 'pink', 'grey78')
+  lisa_labels = c("High-High", "High-Low", "Low-High", "Low-Low", "Not Significant")
+  tms_pal <- levels(x$LISA_sig)[sort(unique(as.integer(x$LISA_sig)))]
+  tms_lab <- lisa_labels[sort(unique(as.integer(x$LISA_sig)))]
   # plot the map
   # Plot out the full set of polygons (missing data will not be overlaid)
-  plot(origPoly %>% st_geometry, col = "white", border = T, lwd = 0.01)
+  #plot(origPoly %>% st_geometry, col = "white", border = T, lwd = 0.01)
   if (plot.only.significant == TRUE){
-    plot(origPoly %>% st_geometry, col=colors1,border=F, add = TRUE) 
+
+    dat_cont <- x %>% filter(STATEFP %in% (fips %>% filter(Contiguous == 1) %>% .$FIPS))
+    dat_ak <- x %>% filter(STATEFP == '02')
+    dat_hi <- x %>% filter(STATEFP == '15')
+
+    #tms_pal <- c('red', 'blue', 'lightblue', 'pink', 'dark grey')
+    tms <- tm_shape(dat_cont) +
+        tm_fill(col = 'LISA_sig', palette = tms_pal, colorNA = 'black', labels = tms_lab, title = variable.name,
+                drop.levels = TRUE, stretch.palette = FALSE) +
+        tm_borders(col = 'transparent') +
+        tm_layout(legend.show = TRUE, legend.position = c('RIGHT', 'BOTTOM'), frame = FALSE)
+    tms_ak <- tm_shape(dat_ak) +
+        tm_fill(col = 'LISA_sig', palette = tms_pal, colorNA = 'black',
+                drop.levels = TRUE, stretch.palette = FALSE) +
+        tm_borders(col = 'transparent') +
+        tm_layout(legend.show = FALSE, frame = FALSE, bg.color = NA)
+    tms_hi <- tm_shape(dat_hi) +
+        tm_fill(col = 'LISA_sig', palette = tms_pal, colorNA = 'black',
+                drop.levels = TRUE, stretch.palette = FALSE) +
+        tm_borders(col = 'transparent') +
+        tm_layout(legend.show = FALSE, frame = FALSE, bg.color = NA)
+    fullpath <- str_c(basepath, variable.name, '.png')
+    if (!is.null(basepath)){
+    png(filename = fullpath,
+        height = 15, width = 24, units = 'cm', res = 300)
+      print(tms)
+      print(tms_hi, vp = grid::viewport(0.25, 0.125, width = 0.15, height = 0.25))
+      print(tms_ak, vp = grid::viewport(0.1, 0.12, width = 0.25, height = 0.25))
+    dev.off()
+    #tmap_save(tm = tms, filename = fullpath,
+    #        height = 15, width = 24, units = 'cm', dpi = 300)
+    } else {
+      print(tms)
+      print(tms_hi, vp = grid::viewport(0.25, 0.125, width = 0.15, height = 0.25))
+      print(tms_ak, vp = grid::viewport(0.1, 0.12, width = 0.25, height = 0.25))
+    }
+    #plot(origPoly %>% st_geometry, col=colors1,border=F, add = TRUE) 
   }else{
-    plot(origPoly %>% st_geometry, col=colors,border=F, add = TRUE)
+    
+    dat_cont <- x %>% filter(STATEFP %in% (fips %>% filter(Contiguous == 1) %>% .$FIPS))
+    dat_ak <- x %>% filter(STATEFP == '02')
+    dat_hi <- x %>% filter(STATEFP == '15')
+
+    #tms_pal <- c('red', 'blue', 'lightblue', 'pink', 'dark grey')
+    tms <- tm_shape(dat_cont) +
+        tm_fill(col = 'LISA_sig', palette = tms_pal, colorNA = 'black', labels = tms_lab,
+                drop.levels = TRUE, stretch.palette = FALSE) +
+        tm_borders(col = 'transparent') +
+        tm_layout(legend.show = TRUE, legend.position = c('RIGHT', 'BOTTOM'), frame = FALSE)
+    tms_ak <- tm_shape(dat_ak) +
+        tm_fill(col = 'LISA_sig', palette = tms_pal, colorNA = 'black',
+                drop.levels = TRUE, stretch.palette = FALSE) +
+        tm_borders(col = 'transparent') +
+        tm_layout(legend.show = FALSE, frame = FALSE, bg.color = NA)
+    tms_hi <- tm_shape(dat_hi) +
+        tm_fill(col = 'LISA_sig', palette = tms_pal, colorNA = 'black',
+                drop.levels = TRUE, stretch.palette = FALSE) +
+        tm_borders(col = 'transparent') +
+        tm_layout(legend.show = FALSE, frame = FALSE, bg.color = NA)
+    fullpath <- str_c(basepath, variable.name, '.png')
+    if (!is.null(basepath)){
+    png(filename = fullpath,
+        height = 15, width = 24, units = 'cm', res = 300)
+      print(tms)
+      print(tms_hi, vp = grid::viewport(0.25, 0.125, width = 0.15, height = 0.25))
+      print(tms_ak, vp = grid::viewport(0.1, 0.12, width = 0.25, height = 0.25))
+    dev.off()
+    #tmap_save(tm = tms, filename = fullpath,
+    #        height = 15, width = 24, units = 'cm', dpi = 300)
+    } else {
+      print(tms)
+      print(tms_hi, vp = grid::viewport(0.25, 0.125, width = 0.15, height = 0.25))
+      print(tms_ak, vp = grid::viewport(0.1, 0.12, width = 0.25, height = 0.25))
+    }
+    #plot(origPoly %>% st_geometry, col=colors,border=F, add = TRUE)
   }
-  legend(legend.location, legend = labels, fill = c("red", "pink", "lightblue", "blue", "grey78", "black"), bty = "n")
+  #legend(legend.location, legend = labels, fill = c("red", "pink", "lightblue", "blue", "grey78", "black"), bty = "n")
 
   
 }
+
+
+## Export plots
+
+
+clw <- nb2listw(poly2nb(county_merge_s), zero.policy = TRUE)
+
+county_merge_s <- county_merge_s %>% 
+  dplyr::select(-87:-89, -92:-93) %>% 
+  mutate_at(.vars = vars(alcohol, incarceration, overdose, black_insurance, readmission,
+                         pop_SNAP, primary),
+            .funs = list(~ifelse(.=='N/A', NA, as.numeric(.))))
+for (i in 5:88){mapping_select(county_merge_s, colnames(county_merge_s)[i], basepath = '/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Maps/Values/')}
+for (i in 5:88){
+  lmid <- lmi_select(county_merge_s, colnames(county_merge_s)[i])
+  #png(str_c('/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Maps/LocalMoranI/', colnames(county_merge_s)[i], '.png'),
+  #    width = 25, height = 16, units = 'cm', res = 300)
+  plot.local.moran(county_merge_s, colnames(county_merge_s)[i], lmid, clw, basepath = '/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Maps/LocalMoranI/')
+  #dev.off()
+  }
+
 
 
 ## purrr nest-unnest: combining weight list into the data.frame design
---
+++ Analysis/NearestNeighbor_Analysis_081520.R
@@ -0,0 +1,88 @@
+### last revision: 081520
+library(pacman)
+p_load(tidyverse, sf, spdep, dtplyr, tmap, rmapshaper, classInt, nngeo)
+
+## Get Data
+# county with attributes
+county_a <- st_read('/mnt/c/Users/sigma/OneDrive/Data/HIV/Yusuf/County/Result/County_Merged_081020.geojson')
+# geocoded alcohol outlets
+alc <- st_read('/mnt/c/Users/sigma/OneDrive/Data/HIV/Geocoding/Geocoding_Premise_Cleaned_Active.shp')
+# PrEP centers
+prep <- st_read('/mnt/c/Users/sigma/OneDrive/Data/HIV/PREP_CDC.gpkg')
+
+alc <- alc %>% st_transform(2163)
+prep <- prep %>% st_transform(2163)
+
+## Classify counties after calculating the rates per 100000
+county_a <- county_a %>% 
+    mutate(r_hiv_c10 = ifelse(r_hiv_new_pop >= quantile(r_hiv_new_pop, 0.9, na.rm = TRUE), 1, 0),
+           r_hiv_c20 = ifelse(r_hiv_new_pop >= quantile(r_hiv_new_pop, 0.8, na.rm = TRUE), 1, 0),
+           r_hiv_c30 = ifelse(r_hiv_new_pop >= quantile(r_hiv_new_pop, 0.7, na.rm = TRUE), 1, 0),
+           r_hiv_c40 = ifelse(r_hiv_new_pop >= quantile(r_hiv_new_pop, 0.6, na.rm = TRUE), 1, 0),
+           r_hiv_c50 = ifelse(r_hiv_new_pop >= quantile(r_hiv_new_pop, 0.5, na.rm = TRUE), 1, 0))
+
+
+county_t <- county_a %>% 
+    filter(state.ab %in% c('NY', 'CT', 'NJ'))
+alc_s <- alc %>% .[county_t %>% st_geometry %>% st_buffer(5000),]
+prep_s <- prep %>% .[county_t %>% st_geometry,]
+
+test_nn <- st_nn(prep, alc, returnDist = TRUE)
+
+calc_nndist <- function(pnt1, pnt2, ref, field){
+    ref_above <- ref %>% 
+        filter(!!rlang::sym(field) == 1 & !is.na(!!rlang::sym(field)))
+    ref_below <- ref %>% 
+        filter(!!rlang::sym(field) == 0 & !is.na(!!rlang::sym(field)))
+
+    pnt1_inabove <- pnt1[ref_above %>% st_geometry,]
+    pnt2_inabove <- pnt2[ref_above %>% st_geometry,]
+    pnt1_inabove_ext <- pnt1[ref_above %>% st_geometry %>% st_buffer(5000),]
+    pnt2_inabove_ext <- pnt2[ref_above %>% st_geometry %>% st_buffer(5000),]
+
+    pnt1_inbelow <- pnt1[ref_below %>% st_geometry,]
+    pnt2_inbelow <- pnt2[ref_below %>% st_geometry,]
+    pnt1_inbelow_ext <- pnt1[ref_below %>% st_geometry %>% st_buffer(5000),]
+    pnt2_inbelow_ext <- pnt2[ref_below %>% st_geometry %>% st_buffer(5000),]
+
+
+    dist12_above <- st_nn(pnt1_inabove, pnt2_inabove_ext, returnDist = TRUE) %>% 
+        .$dist %>% 
+        do.call(c,.) %>% 
+        mean
+    dist12_below <- st_nn(pnt1_inbelow, pnt2_inbelow_ext, returnDist = TRUE) %>% 
+        .$dist %>% 
+        do.call(c,.) %>% 
+        mean
+    dist21_above <- st_nn(pnt2_inabove, pnt1_inabove_ext, returnDist = TRUE) %>% 
+        .$dist %>% 
+        do.call(c,.) %>% 
+        mean
+    dist21_below <- st_nn(pnt2_inbelow, pnt1_inbelow, returnDist = TRUE) %>% 
+        .$dist %>% 
+        do.call(c,.) %>% 
+        mean
+    dist.check <- data.frame(D12_Above = dist12_above,
+                             D21_Above = dist21_above,
+                             D12_Below = dist12_below,
+                             D21_Below = dist21_below)
+    #dist.check <- list(dist12_above, dist12_below, dist21_above, dist21_below)
+    return(dist.check)
+}
+
+nndist_c10 <- calc_nndist(alc_s, prep_s, county_t, 'r_hiv_c10')
+nndist_c20 <- calc_nndist(alc_s, prep_s, county_t, 'r_hiv_c20')
+nndist_c30 <- calc_nndist(alc_s, prep_s, county_t, 'r_hiv_c30')
+nndist_c40 <- calc_nndist(alc_s, prep_s, county_t, 'r_hiv_c40')
+nndist_c50 <- calc_nndist(alc_s, prep_s, county_t, 'r_hiv_c50')
+
+nndists <- bind_rows(
+    nndist_c10,
+    nndist_c20,
+    nndist_c30,
+    nndist_c40,
+    nndist_c50
+) %>% 
+    mutate(percentage = str_c(seq(10,50,10), '%'), Var1 = 'Alcohol', Var2 = 'PrEP')
+
+nndists %>% write_csv('/mnt/c/Users/sigma/OneDrive/Data/HIV/NearestNeighborDistance_081520.csv')
